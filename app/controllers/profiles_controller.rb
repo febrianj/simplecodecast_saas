5e9efa925a6822b40bacc7565b76c00a9bfa2116
Febrian <febrian.jiuwira@gmail.com>
2016-03-19 18:06:19 +0700

    Add user profile barebones

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,7 @@
 class ProfilesController < ApplicationController
   def new
   	@user = User.find(params[:user_id])
-  	@profile = @user.build_profile
+    @profile = @user.build_profile
   end
 
   def create
@@ -18,6 +18,6 @@ class ProfilesController < ApplicationController
 
   private
   	def profile_params
-  	  params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+  	  params.require(:profile).permit(:user_id, :first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
   	end
 end
